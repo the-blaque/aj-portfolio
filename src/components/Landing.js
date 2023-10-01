45f0fcaeb5b61ca14052315e8773ea46638d2c77
Oluwaseeni Ajayi <42158213+the-blaque@users.noreply.github.com>
2023-09-30 18:01:11 -0600

    landing page first push

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Lottie from 'lottie-react';
+import animationData from '../assets/1qTKdvp3K9.json';
 import '../styles/Landing.css';
 
 function Landing() {
   return (
     <div className="landing-container">
-      <h1>Welcome</h1>
+      <Lottie animationData={animationData} className="lottie-animation" loop={false} speed={2.5} />
       <h2 className="name">I am Oluwaseeni Ajayi</h2>
       <p className="instructions">Please scroll to explore</p>
     </div>
