9519cfc97d029bb9638ae781a2395c22db15acb4
Oluwaseeni Ajayi <42158213+the-blaque@users.noreply.github.com>
2023-10-04 12:57:05 -0600

    incorporating tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
